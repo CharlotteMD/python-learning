067c459a202441ebaa8403d102a64334b34d12d1
Charlotte Qazi <charlottemdavies@hotmail.co.uk>
2021-11-10 16:54:35 +0000

    learning to build an api

---
+++ build_api/api.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+STUDENTS = {
+  '1': {'name': 'Mark', 'age': 23, 'spec': 'math'},
+  '2': {'name': 'Jane', 'age': 20, 'spec': 'biology'},
+  '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
+  '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
+}
+
+  class StudentsList(Resource):
+  def get(self);
+  def post(self):
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
+api.add_resource(StudentsList, '/students/')
